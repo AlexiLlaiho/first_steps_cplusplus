45d30987cbf77a7960db270793291ba1e36b0128
alexi <alexi@alexi-ASUS-TUF-Dash>
2023-03-12 21:54:50 +0300

    task2

--- cprog_main.cpp
+++ cprog_main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
+#include <math.h>
 
 int main(){
-    std::cout <<"C++ is a general-purpose programming language with a bias towards systems programming that\n";    
-    std::cout << "  - is a better C\n";
-    std::cout << "  - supports data abstraction\n";
-    std::cout << "  - supports object-oriented programming\n";
-    std::cout << "  - supports generic programming.\n";
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << (a+b) << std::endl;
     return 0;
 }
