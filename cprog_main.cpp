893fea09d1c49cad8aaf37235487230a56ee805d
algorbunow <a.gorbunov@indrones.ru>
2023-03-17 12:37:52 +0300

    task 1.4.7

--- cprog_main.cpp
+++ cprog_main.cpp
@@ -2,8 +2,16 @@
 #include <math.h>
 #include <cstdint>
 
-int main(){
-uint64_t n;
-    std::cin >> n;
-    std::cout << n * (n + 1) / 2 << "\n";
+int main()
+{
+    int x1, y1, x2, y2;    
+    while(std::cin >> x1 >> y1 >> x2 >> y2){
+        if((x2 == x1) || (y2 == y1) || (abs(x2 - x1) == abs(y2 - y1))){
+            std::cout << "YES" << std::endl;
+        }
+        else{
+            std::cout << "NO" << std::endl;
+        }
+    }
+    return 0;    
 }
