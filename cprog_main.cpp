77288e3e0274d67eec87c6186e401309ad9f8df4
algorbunow <a.gorbunov@indrones.ru>
2023-03-18 14:18:04 +0300

    task 1.4.7.2

--- cprog_main.cpp
+++ cprog_main.cpp
@@ -2,16 +2,41 @@
 #include <math.h>
 #include <cstdint>
 
+void sortFunc(unsigned int& a2, unsigned int& b2, unsigned int& c2);
+
 int main()
 {
-    int x1, y1, x2, y2;    
-    while(std::cin >> x1 >> y1 >> x2 >> y2){
-        if((x2 == x1) || (y2 == y1) || (abs(x2 - x1) == abs(y2 - y1))){
-            std::cout << "YES" << std::endl;
+    unsigned int a, b, c;    
+    while(std::cin >> a >> b >> c){
+    sortFunc(a, b, c);
+        if((a+b) > c){
+            if(pow(a,2) + pow(b,2) == pow(c,2)){
+                std::cout << "YES" << std::endl;    
+            } else{
+                std::cout << "NO" << std::endl;
+            }            
         }
         else{
-            std::cout << "NO" << std::endl;
+            std::cout << "UNDEFINED" << std::endl;
         }
     }
     return 0;    
+}
+
+void sortFunc(unsigned int& a2, unsigned int& b2, unsigned int& c2){
+    unsigned int local_max;
+        if(b2 > a2){
+            if(c2 < b2){
+                local_max = b2;
+                b2 = c2;
+                c2 = local_max; 
+            }
+        }          
+        else{
+            if(c2 < a2){
+                local_max = a2;
+                a2 = c2;
+                c2 = local_max;
+            }
+        }
 }
