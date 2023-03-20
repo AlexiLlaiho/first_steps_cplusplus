797d71dbef141e665a44a65ff3ed3cb05f4107fa
algorbunow <a.gorbunov@indrones.ru>
2023-03-20 10:28:24 +0300

    task 1.4.7.3

--- cprog_main.cpp
+++ cprog_main.cpp
@@ -2,41 +2,20 @@
 #include <math.h>
 #include <cstdint>
 
-void sortFunc(unsigned int& a2, unsigned int& b2, unsigned int& c2);
-
 int main()
 {
-    unsigned int a, b, c;    
-    while(std::cin >> a >> b >> c){
-    sortFunc(a, b, c);
-        if((a+b) > c){
-            if(pow(a,2) + pow(b,2) == pow(c,2)){
-                std::cout << "YES" << std::endl;    
-            } else{
-                std::cout << "NO" << std::endl;
-            }            
+    unsigned int year;    
+    while(std::cin >> year){    
+        if(year % 400 == 0){            
+            std::cout << "YES" << std::endl;
         }
-        else{
-            std::cout << "UNDEFINED" << std::endl;
+        else if(year % 100 == 0){
+            std::cout << "NO" << std::endl;
         }
+        else if(year % 4 == 0)
+            std::cout << "YES" << std::endl;
+        else
+            std::cout << "NO" << std::endl;
     }
     return 0;    
-}
-
-void sortFunc(unsigned int& a2, unsigned int& b2, unsigned int& c2){
-    unsigned int local_max;
-        if(b2 > a2){
-            if(c2 < b2){
-                local_max = b2;
-                b2 = c2;
-                c2 = local_max; 
-            }
-        }          
-        else{
-            if(c2 < a2){
-                local_max = a2;
-                a2 = c2;
-                c2 = local_max;
-            }
-        }
 }
