7da7273bb34980f3f585a80e1119d0d56e6a7bf8
algorbunow <a.gorbunov@indrones.ru>
2023-03-21 10:35:55 +0300

    task 1.4.7.4

--- cprog_main.cpp
+++ cprog_main.cpp
@@ -4,18 +4,26 @@
 
 int main()
 {
+    unsigned int index;
+    unsigned int month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     unsigned int year;    
-    while(std::cin >> year){    
-        if(year % 400 == 0){            
-            std::cout << "YES" << std::endl;
+    while(std::cin >> index >> year){    
+        if(year % 400 == 0){
+            if(index == 2)            
+                std::cout << month[index]+1 << std::endl; //yes
+            else
+                std::cout << month[index] << std::endl; //yes
         }
         else if(year % 100 == 0){
-            std::cout << "NO" << std::endl;
+            std::cout << month[index] << std::endl; //no
         }
         else if(year % 4 == 0)
-            std::cout << "YES" << std::endl;
+            if(index == 2)            
+                std::cout << month[index]+1 << std::endl; //yes
+            else
+                std::cout << month[index] << std::endl; //yes
         else
-            std::cout << "NO" << std::endl;
+             std::cout << month[index] << std::endl; //no
     }
     return 0;    
 }
